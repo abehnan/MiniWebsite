467680881e43549ca5a42b9983a0db94e768f203
abehnan <abehnan@gmail.com>
2017-10-18 18:24:29 -0400

    added background color button

--- README.md
+++
@@ -1,8 +0,0 @@
-# MiniWebsite
-Website Assignment
-
-Alexander Behnan
-260639146
-
-
-Browser used: latest version of google chrome
---
+++ a4-5.js
@@ -0,0 +1,21 @@
+function clickMe() {
+    var inputColor = prompt("Enter a color name for the " + "background of this page", "");
+    if (inputColor == 'red') {
+        document.getElementById("loginbar").style.backgroundColor = 'rgb(77, 4, 4)';
+        document.getElementById("titlebar").style.backgroundColor = 'rgb(170, 23, 23)';
+        document.getElementById("menubar").style.backgroundColor = 'rgb(206, 73, 73)';
+        document.getElementById("bottombox").style.backgroundColor = 'rgb(77, 4, 4)';
+        document.getElementById("categories").style.backgroundColor = '#994747';
+    }
+    else if (inputColor == 'blue') {
+        document.getElementById("loginbar").style.backgroundColor = '#15315e';
+        document.getElementById("titlebar").style.backgroundColor = '#145bce';
+        document.getElementById("menubar").style.backgroundColor = '#588de2';
+        document.getElementById("bottombox").style.backgroundColor = '#15315e';
+        document.getElementById("categories").style.backgroundColor = '#90b1e5';
+    }
+    else {
+        alert("The only accepted colors are: 'red' or 'blue'");
+
+    }
+}
