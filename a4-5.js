c32b26e90ca87ce04a9f3ea4856fe091d377b462
abehnan <abehnan@gmail.com>
2017-10-18 19:11:33 -0400

    canvas area now changes colors

--- a4-5.js
+++ a4-5.js
@@ -5,13 +5,16 @@ function clickMe() {
         document.getElementById("titlebar").style.backgroundColor = 'rgb(170, 23, 23)';
         document.getElementById("menubar").style.backgroundColor = 'rgb(206, 73, 73)';
         document.getElementById("bottombox").style.backgroundColor = 'rgb(77, 4, 4)';
+        document.getElementById("canvasbox").style.backgroundColor = 'rgb(77, 4, 4)';
         document.getElementById("categories").style.backgroundColor = '#994747';
+        
     }
     else if (inputColor == 'blue') {
         document.getElementById("loginbar").style.backgroundColor = '#15315e';
         document.getElementById("titlebar").style.backgroundColor = '#145bce';
         document.getElementById("menubar").style.backgroundColor = '#588de2';
         document.getElementById("bottombox").style.backgroundColor = '#15315e';
+        document.getElementById("canvasbox").style.backgroundColor = '#15315e';
         document.getElementById("categories").style.backgroundColor = '#90b1e5';
     }
     else {
