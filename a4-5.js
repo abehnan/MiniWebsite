4e7df4c23f6e56d7e95224d82c475ad9981340ba
abehnan <abehnan@gmail.com>
2017-10-20 20:11:20 -0400

    added green color option

--- a4-5.js
+++ a4-5.js
@@ -1,5 +1,5 @@
 function clickMe() {
-    var inputColor = prompt("Please enter a new background color:\nThe accepted options are 'red' or 'blue'.", "");
+    var inputColor = prompt("Please enter a new background color:\nThe accepted options are 'green', 'red' or 'blue'.", "");
     if (inputColor == 'red') {
         document.getElementById("loginbar").style.backgroundColor = 'rgb(77, 4, 4)';
         document.getElementById("titlebar").style.backgroundColor = 'rgb(170, 23, 23)';
@@ -17,8 +17,16 @@ function clickMe() {
         document.getElementById("canvasbox").style.backgroundColor = '#15315e';
         document.getElementById("categories").style.backgroundColor = '#90b1e5';
     }
+    else if (inputColor == 'green') {
+        document.getElementById("loginbar").style.backgroundColor = 'rgb(0, 65, 65)';
+        document.getElementById("titlebar").style.backgroundColor = '#1F8061';
+        document.getElementById("menubar").style.backgroundColor = 'rgb(107, 168, 121)';
+        document.getElementById("bottombox").style.backgroundColor = 'rgb(0, 65, 65)';
+        document.getElementById("canvasbox").style.backgroundColor = 'rgb(0, 65, 65)';
+        document.getElementById("categories").style.backgroundColor = 'rgb(170, 182, 159)';
+    }
     else {
-        alert("The only accepted colors are: 'red' or 'blue'");
+        alert("The only accepted colors are: 'green', 'red' or 'blue'");
 
     }
 }
